810e8af514d173a442463f95531a6f2fb365f0ca
Chris Eldon <chris_eldon@yahoo.com>
2024-11-27 13:01:14 +0700

    fix: revert back to ffmpeg and add instruction to enable it

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-sudo apt install pandoc
+1. run `sudo apt install pandoc ffmpeg`
+2. copy ffmpeg from `usr/bin/ffmpeg` to `ezra-be/venv/bin/`
+    ```
+    cp -r /usr/bin/ffmpeg /root/ezra-be/venv/bin/ffmpeg
+    ```
--- whisper.py
+++ whisper.py
@@ -1,7 +1,6 @@
 import time
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-import soundfile
 
 
 def measure_execution_time(func):
@@ -44,8 +43,4 @@ class Whisper:
 
     @measure_execution_time
     def transcribe(self, file_path):
-        data, samplerate = soundfile.read(file_path)
-        if len(data.shape) > 1:
-            # Convert stereo to mono by averaging channels
-            data = data.mean(axis=1)
-        return self.pipe(data)
+        return self.pipe(file_path)
